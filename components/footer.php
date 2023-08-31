eacdd0c58f33efd146317afbc602a9c2328c6024
Gaurav_Singh <79020657+mrmister28@users.noreply.github.com>
2023-08-31 21:10:40 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,43 @@
+<footer class="footer">
+
+   <section class="grid">
+
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:+91 0123456789"><i class="fas fa-phone"></i> +91 0123456789</a>
+         <a href="tel:+91 0123456789"><i class="fas fa-phone"></i> +91 0123456789</a>
+         <a href="mailto:123@gmail.com"><i class="fas fa-envelope"></i> 123@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Jamshedpur, Jharkhand, India - 831015 </a>
+      </div>
+
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://twitter.com/"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="https://in.linkedin.com/"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>ElectraMarket. </span> | all rights reserved!</div>
+
+</footer>
